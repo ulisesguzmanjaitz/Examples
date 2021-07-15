0e557c6932e1267fd16010b00afd4642b6ea64d2
ulisesguzmanjaitz <58351339+ulisesguzmanjaitz@users.noreply.github.com>
2021-07-14 23:44:21 -0300

    Add files via upload

---
+++ Ejercicios JAVA/Cuenta/src/cuenta/Cuenta.java
@@ -0,0 +1,58 @@
+package cuenta;
+
+public class Cuenta {
+
+    private String titular;
+    private double cantidad;
+
+    public Cuenta(String titular) {
+        this(titular, 0); //Sobrecarga
+    }
+
+    public Cuenta(String titular, double cantidad) {
+        this.titular = titular;
+        //Si la cantidad es menor que cero, lo ponemos a cero
+        if (cantidad < 0) {
+            this.cantidad = 0;
+        } else {
+            this.cantidad = cantidad;
+        }
+    }
+
+    //Metodos
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            this.cantidad += cantidad;
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (this.cantidad - cantidad < 0) {
+            this.cantidad = 0;
+        } else {
+            this.cantidad -= cantidad;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "El titular " + titular + " tiene " + cantidad + " pesos en la cuenta";
+    }
+
+}
---
+++ Ejercicios JAVA/Cuenta/src/cuenta/Test.java
@@ -0,0 +1,19 @@
+package cuenta;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Cuenta cuenta_1 = new Cuenta("Hi");
+        Cuenta cuenta_2 = new Cuenta("Juan", 300);
+
+        cuenta_1.ingresar(300);
+        cuenta_2.ingresar(400);
+
+        cuenta_1.retirar(500);
+        cuenta_2.retirar(100);
+
+        System.out.println(cuenta_1);
+        System.out.println(cuenta_2);
+    }
+
+}
